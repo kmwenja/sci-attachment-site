bee75e72bffab16a151bfbf4edbd276086964e85
caninemwenja <mwenja07@gmail.com>
2013-08-17 23:12:42 +0300

    refactored signin to controller

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ $user_session_controller = new UserSessionController();
 
 $app->get('/signout', 'signout')->name('signout');
 $app->get('/home', 'home')->name('home');
-$app->post('/signin', 'signin')->name('signin');
+$app->post('/signin', array($user_session_controller,'signin'))->name('signin');
 $app->get('/signin', array($user_session_controller,'signin_form'))->name('signin_form');
 $app->get('/welcome', 'welcome');
 $app->get('/', 'welcome')->name('welcome');
--- views/session.php
+++ views/session.php
@@ -12,6 +12,31 @@ class UserSessionController
 	public function signin_form(){
 		$this->app->render('signin.html.twig');
 	}
+
+	public function signin(){
+		$username = $this->app->request()->post('username');
+		$password = $this->app->request()->post('password');
+
+		$users = ORM::for_table('user')->where('username', $username)->find_many();
+
+		if(count($users) !== 1){
+			$this->app->flash('error','invalid account');
+			$this->app->redirect($this->app->urlFor('signin_form'));
+			return;
+		}
+
+		if($users[0]->password !== $password){
+			$this->app->flash('error', 'invalid password');
+			$this->app->redirect($this->app->urlFor('signin_form'));
+			return;
+		}
+
+		$_SESSION['username'] = $users[0]->username;
+		$_SESSION['name'] = $users[0]->name;
+		$_SESSION['signed_in'] = True;
+
+		$this->app->redirect($this->app->urlFor('home'));
+	}
 }
 
 function signin_form(){
