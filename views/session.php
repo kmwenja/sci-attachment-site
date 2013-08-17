9f213293830b2f05139c7639187efc012c8e71ee
caninemwenja <mwenja07@gmail.com>
2013-08-17 23:19:17 +0300

    removed former functions and cleaned up a bit

--- views/session.php
+++ views/session.php
@@ -9,11 +9,13 @@ class UserSessionController
 		$this->app = \Slim\Slim::getInstance();
 	}
 
-	public function signin_form(){
+	public function signin_form()
+	{
 		$this->app->render('signin.html.twig');
 	}
 
-	public function signin(){
+	public function signin()
+	{
 		$username = $this->app->request()->post('username');
 		$password = $this->app->request()->post('password');
 
@@ -38,7 +40,8 @@ class UserSessionController
 		$this->app->redirect($this->app->urlFor('home'));
 	}
 
-	public function home(){
+	public function home()
+	{
 		if(!($_SESSION and isset($_SESSION['signed_in']) and $_SESSION['signed_in'] === True)){
 			$this->app->redirect($this->app->urlFor('signin_form'));
 			return;
@@ -47,7 +50,8 @@ class UserSessionController
 		$this->app->render('home.html.twig');
 	}
 
-	public function signout(){
+	public function signout()
+	{
 		unset($_SESSION['username']);
 		unset($_SESSION['name']);
 		unset($_SESSION['signed_in']);
@@ -56,58 +60,4 @@ class UserSessionController
 
 		$this->app->redirect($this->app->urlFor('signin_form'));
 	}
-}
-
-function signin_form(){
-	$app = \Slim\Slim::getInstance();
-	$app->render('signin.html.twig');
-}
-
-function signin(){
-	$app = \Slim\Slim::getInstance();
-
-	$username = $app->request()->post('username');
-	$password = $app->request()->post('password');
-
-	$users = ORM::for_table('user')->where('username', $username)->find_many();
-
-	if(count($users) !== 1){
-		$app->flash('error','invalid account');
-		$app->redirect($app->urlFor('signin_form'));
-		return;
-	}
-
-	if($users[0]->password !== $password){
-		$app->flash('error', 'invalid password');
-		$app->redirect($app->urlFor('signin_form'));
-		return;
-	}
-
-	$_SESSION['username'] = $users[0]->username;
-	$_SESSION['name'] = $users[0]->name;
-	$_SESSION['signed_in'] = True;
-
-	$app->redirect($app->urlFor('home'));
-}
-
-function home(){
-	$app = \Slim\Slim::getInstance();
-	if(!($_SESSION and isset($_SESSION['signed_in']) and $_SESSION['signed_in'] === True)){
-		$app->redirect($app->urlFor('signin_form'));
-		return;
-	}
-
-	$app->render('home.html.twig');
-}
-
-function signout(){
-	$app = \Slim\Slim::getInstance();
-
-	unset($_SESSION['username']);
-	unset($_SESSION['name']);
-	unset($_SESSION['signed_in']);
-
-	session_destroy();
-
-	$app->redirect($app->urlFor('signin_form'));
 }
